72a53e4449e308a41a32143037449ea1946921dd
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-01-21 12:59:07 +0100

    Classes implementades, closes #1

---
+++ code/T3-PR1 ConsoleApp/classes/SistemaHidroelectric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace T3PR1
+{
+    public class SistemaHidroelectric : SistemaEnergia
+    {
+        public double Cabal { get; set; }
+
+        public SistemaHidroelectric(DateTime data, double cabal)
+        {
+            Data = data;
+            Tipus = "Hidroelèctrica";
+            Cabal = cabal;
+        }
+    }
+}
---
+++ code/T3-PR1 ConsoleApp/classes/SistemaSolar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace T3PR1
+{
+    public class SistemaSolar : SistemaEnergia
+    {
+        public int HoresSol { get; set; }
+
+        public SistemaSolar(DateTime data, int horesSol)
+        {
+            Data = data;
+            Tipus = "Solar";
+            HoresSol = horesSol;
+        }
+    }
+}

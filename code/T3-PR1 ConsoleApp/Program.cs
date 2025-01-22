9f1692e898a92c467eb650d1f0a47e320dde4863
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-01-22 13:03:43 +0100

    Feina a classe yk

--- code/T3-PR1 ConsoleApp/Program.cs
+++ code/T3-PR1 ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using T3PR1;
 
 namespace T3PR1
 {
@@ -6,32 +7,33 @@ namespace T3PR1
     {
         public static void Main()
         {
-            const string MissatgeBenvinguda = "\r\n _____          _____                             _____       _       _   _                 \r\n|  ___|        |  ___|                           /  ___|     | |     | | (_)                \r\n| |__  ___ ___ | |__ _ __   ___ _ __ __ _ _   _  \\ `--.  ___ | |_   _| |_ _  ___  _ __  ___ \r\n|  __|/ __/ _ \\|  __| '_ \\ / _ \\ '__/ _` | | | |  `--. \\/ _ \\| | | | | __| |/ _ \\| '_ \\/ __|\r\n| |__| (_| (_) | |__| | | |  __/ | | (_| | |_| | /\\__/ / (_) | | |_| | |_| | (_) | | | \\__ \\\r\n\\____/\\___\\___/\\____/_| |_|\\___|_|  \\__, |\\__, | \\____/ \\___/|_|\\__,_|\\__|_|\\___/|_| |_|___/\r\n                                     __/ | __/ |                                            \r\n                                    |___/ |___/                                             \r\n";
+            int contadorSimulacions = 0;
             bool sortirPrograma = false;
 
             do
             {
-                Console.WriteLine(MissatgeBenvinguda);
+                Console.WriteLine(Missatges.MissatgeBenvinguda);
                 SistemaUtils.MostrarMenu();
 
                 switch (SistemaUtils.LlegirNumeroValid())
                 {
                     case 1:
-                        Console.WriteLine("Has escollit la opció 1");
+                        Console.WriteLine(Missatges.OpcioUnMenuMissatge);
+                        contadorSimulacions++;
                         break;
                     case 2:
-                        Console.WriteLine("Has escollit la opció 2");
+                        Console.WriteLine(contadorSimulacions > 0 ? Missatges.OpcioDosValidMenuMissatge : Missatges.OpcioDosInvalidMenuMissatge);
                         break;
                     case 3:
-                        Console.WriteLine("Has escollit sortir del programa! :(");
+                        Console.WriteLine(Missatges.OpcioTresMenuMissatge);
                         sortirPrograma = true;
                         break;
                     default:
-                        Console.Write("Error: si us plau, introdueix un número vàlid.");
+                        Console.Write(Missatges.OpcioEquivocadaMenuMissatge);
                         break;
                 }
             } while (!sortirPrograma);
-            
+
         }
-    }
+    } 
 }
---
+++ code/T3-PR1_ClassLibrary/Missatges.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace T3PR1
+{
+    public class Missatges
+    {
+        public const string MissatgeBenvinguda = "\r\n _____          _____                             _____       _       _   _                 \r\n|  ___|        |  ___|                           /  ___|     | |     | | (_)                \r\n| |__  ___ ___ | |__ _ __   ___ _ __ __ _ _   _  \\ `--.  ___ | |_   _| |_ _  ___  _ __  ___ \r\n|  __|/ __/ _ \\|  __| '_ \\ / _ \\ '__/ _` | | | |  `--. \\/ _ \\| | | | | __| |/ _ \\| '_ \\/ __|\r\n| |__| (_| (_) | |__| | | |  __/ | | (_| | |_| | /\\__/ / (_) | | |_| | |_| | (_) | | | \\__ \\\r\n\\____/\\___\\___/\\____/_| |_|\\___|_|  \\__, |\\__, | \\____/ \\___/|_|\\__,_|\\__|_|\\___/|_| |_|___/\r\n                                     __/ | __/ |                                            \r\n                                    |___/ |___/                                             \r\n";
+        public const string OpcioUnMenuMissatge = "Iniciant la simulació... que comenci la màgia energètica!",
+            OpcioDosValidMenuMissatge = "Aquí tens les teves simulacions. Atenció, podries acabar creant una nova central elèctrica! ",
+            OpcioDosInvalidMenuMissatge = "Ah, vols veure el futur? Doncs encara no tenim ni un sol dada. Fes la primera simulació i comencem!",
+            OpcioTresMenuMissatge = "Fins aviat! Torna quan vulguis il·luminar el teu dia amb simulacions",
+            OpcioEquivocadaMenuMissatge = "Error: si us plau, introdueix un número vàlid."; 
+    }
+}
